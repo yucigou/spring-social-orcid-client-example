8d996f2754961058b4f49676e8b126197225be5b
Yuci Gou <yuci.gou@gmail.com>
2016-03-25 22:47:39 +0000

    Update controller, home and welcome page for ORCID users

--- spring-social-example/src/main/java/com/credera/springsocial/HomeController.java
+++ spring-social-example/src/main/java/com/credera/springsocial/HomeController.java
@@ -1,19 +1,14 @@
 package com.credera.springsocial;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 import javax.inject.Inject;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.social.connect.Connection;
-import org.springframework.social.facebook.api.Facebook;
-import org.springframework.social.facebook.api.Page;
-import org.springframework.social.twitter.api.Tweet;
-import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.orcid.api.OrcidApi;
+import org.springframework.social.orcid.jaxb.beans.OrcidProfile;
+import org.springframework.social.orcid.utils.StringUtility;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,11 +21,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	@Inject
-    @Qualifier("facebook")
-    Connection<Facebook> facebook;
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+    @Qualifier("orcid")
+    Connection<OrcidApi> orcid;
+		
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		return "home";
@@ -38,29 +31,28 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(Locale locale, Model model) {
-		Facebook fb = facebook.getApi();
-
-		List<Page> moviePages = fb.likeOperations().getMovies();
+	    OrcidApi orcidApi = orcid.getApi();
+	    OrcidProfile orcidProfile = orcidApi.messageOperations().getOrcidProfile();
+	    orcidProfile.getOrcidBio().getPersonalDetails();
+	    
+	    String orcidId = orcidProfile.getOrcidIdentifier().getPath();
+        
+        String givenName = orcidProfile.getOrcidBio().getPersonalDetails().getGivenNames();
+        String familyName = orcidProfile.getOrcidBio().getPersonalDetails().getFamilyName();
+        String displayName = givenName;
+        if (StringUtility.hasContent(familyName)) {
+            if (StringUtility.hasContent(displayName)) {
+                displayName += " ";
+                displayName += familyName;
+            } else {
+                displayName = familyName;
+            }
+        }       
 		
-		List<String> movies = new ArrayList<String>();
-		for (Page p : moviePages) {
-			movies.add(p.getName());
-		}
+		model.addAttribute("orcidId", orcidId);
+		model.addAttribute("name", displayName);
 		
-		// Uncomment the following line to see how easy posting to your timeline is.
-		//fb.feedOperations().post(userId, "I just tried out the My Movies example site!");
-		
-		int numFriends = fb.friendOperations().getFriends().size();
-		
-		model.addAttribute("facebook", true);
-		model.addAttribute("numFriends", numFriends);
-		String name = facebook.getDisplayName();
-		model.addAttribute("name", name);
-		String imageUrl = facebook.getImageUrl();
-		model.addAttribute("imgUrl", imageUrl);
-		model.addAttribute("movies", movies);
-		
-		return "welcome_user";
+		return "welcome_orcid";
 	}
 	
 }
