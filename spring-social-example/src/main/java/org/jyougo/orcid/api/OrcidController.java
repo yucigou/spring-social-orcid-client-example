39aa72601e176e8c03a7b17105180ec994c37390
Yuci Gou <yuci.gou@gmail.com>
2016-06-27 23:04:45 +0100

    Add a new REST API for adding works.

--- spring-social-example/src/main/java/org/jyougo/orcid/api/OrcidController.java
+++ spring-social-example/src/main/java/org/jyougo/orcid/api/OrcidController.java
@@ -2,7 +2,9 @@ package org.jyougo.orcid.api;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.transform.dom.DOMSource;
 
+import org.jyougo.utils.Utils;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.social.connect.Connection;
@@ -12,9 +14,13 @@ import org.springframework.social.orcid.jaxb.beans.OrcidProfile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.Document;
 
 /**
  * Handles requests for API.
+ * 
+ * @author Yuci
+ *
  */
 @RestController
 @RequestMapping("api")
@@ -49,4 +55,39 @@ public class OrcidController {
         return orcidProfile;
     }    
 
+    @RequestMapping(value = "/orcid/add/works", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+    public Boolean addWorks(HttpServletRequest request) {
+        System.out.println("Entering addWorks()");
+        
+        if (orcid == null) {
+            System.out.println("ORCID connection null");
+            return false;
+        }
+
+        OrcidApi orcidApi = orcid.getApi();
+        if (orcidApi == null) {
+            System.out.println("orcidApi null");
+            return false;
+        }
+        
+        MessageOperations messageOperations = orcidApi.messageOperations();
+        if (messageOperations == null) {
+            System.out.println("messageOperations null");
+            return false;
+        }
+        
+        String s = Utils._getWorksXml();
+        Document doc;
+        DOMSource domSource;
+        try {
+            doc = Utils.stringToDom(s);
+            domSource = Utils.getDomSource(doc);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+            return false;
+        }
+        
+        boolean ret = messageOperations.addWorks(orcid.getKey().getProviderUserId(), domSource);
+        return ret;
+    }
 }
